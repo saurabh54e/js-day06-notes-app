686ab1c8381b593e8894bb1fba1f7e54614f6883
saurabh singh <saurabhsingh@saurabhs-Mac.local>
2026-02-25 01:01:39 +0530

    Working Improved

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 
-createBtn.addEventListener("click", function () {
 
+
+
+// save Notes to local storage
+function saveNotes() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+
+
+notesContainer.addEventListener("focusout", function (e) {
+  if (!e.target.classList.contains("input-box")) return;
+
+  const note = e.target.parentElement;
+
+  // if note already removed (delete click)
+  if (!note.isConnected) return;
+
+  if (e.target.innerText.trim() === "") {
+    note.remove();
+    saveNotes();
+  }
+});
+
+// Load notes from local storage
+function loadNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes") || "";
+}
+loadNotes();
+
+
+
+
+const last = notesContainer.querySelector(".input-box:last-child");
+if (last) last.focus();
+
+
+
+// Create new note
+createBtn.addEventListener("click", function () {
   const note = document.createElement("div");
   note.className = "note";
 
-  const inputbox = document.createElement("div"); // ✅ changed
+  const inputbox = document.createElement("div");
   inputbox.className = "input-box";
   inputbox.contentEditable = "true";
 
@@ -15,13 +53,30 @@ createBtn.addEventListener("click", function () {
   img.className = "delete";
 
   note.appendChild(inputbox);
-  note.appendChild(img);   
+  note.appendChild(img);
   notesContainer.appendChild(note);
 
   inputbox.focus();
+  saveNotes();
 });
-const deletebtn = document.querySelector(".delete");
-deletebtn.addEventListener("click", function (e) {
-inputbox.remove();
-})
 
+
+
+// Delete
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+    saveNotes();
+  }
+});
+
+// notesContainer.addEventListener("keydown", function(e){
+//     if(e.key === "Enter" && e.target.classList.contains("input-box")){
+//         e.preventDefault(); // prevent new line
+//         createBtn.click(); // trigger create button click
+//     }
+// })
+
+
+//  Auto save while typing
+notesContainer.addEventListener("input", saveNotes);
